42482f492e343f651b9fecc4a168c3f60818d98f
david <burkhart.s.d@gmail.com>
2017-05-16 16:17:39 -0500

    Working grid, working to get buttons to work next.

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,36 @@
 $(document).ready(function() {
-	var $gridSegment = $("#gridSegment");
-	for (var i = 1; i < 18; i++) {
-		$("#gridSegment").append("<div class='segment' id='gridSegment'></div>");
+	function createGrid(size) {
+		for (var i = 0; i < size; i++) {
+			var newRow = $('<div class="row"></div>');
+			$('#container').append(newRow);
+		}
+		for (var j = 0; j < size; j++) {
+			var newColumn = $('<div class="column"></div>');
+			$('.row').append(newColumn);
+		}
 	}
-})
+
+	function colorChange() {
+		$(document).on('mouseover', '.column', function() {
+			$(this).css('background-color', '#b9def7');
+		});
+	}
+
+	function colorSelect() {
+		$(document).on('click', '#colorPicker', function() {
+			alert($colorMenu);
+		})
+	}
+
+
+
+
+	var originalSize = 16;
+	createGrid(originalSize);
+
+
+	colorChange();
+	colorSelect();
+
+});
+
